831dcce398ba381bf0aa22d7cae7b02edb77f0c7
MarkGraebener <mark.w.graebener@gmail.com>
2022-02-15 20:55:29 -0600

    data types

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,35 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        long myFavoriteNumber = 22/7L; // why no decimal in long? why no work in float?
+        String myString = "Matthew";
+
+//        int x = 5;
+//        System.out.println(x++); // executes after logging
+//        System.out.println(x);  // logs executed code
+
+//        int x = 5;
+//        System.out.println(++x); // executes before logged
+//        System.out.println(x);
+//        System.out.println(myFavoriteNumber + "\n" + myString);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) 3;
+//        System.out.println(three);
+
+//        int x = 4;
+//        System.out.println(x = x + 5); //?????
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x; // ?????
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x; ???
     }
 }
