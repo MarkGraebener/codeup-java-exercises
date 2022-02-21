c708749958e4bb893622a91cb009ce9aaf80df50
MarkGraebener <mark.w.graebener@gmail.com>
2022-02-21 13:13:00 -0600

    methods complete

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,32 +1,76 @@
+import java.util.Scanner;
+
 public class MethodsExercises
 {
-    public static int add(int x,int y)
-    {
-        return x + y;
-    }
-    public static int sub(int x,int y)
-    {
-        return x - y;
-    }
-    public static void multiply(int x,int y)
-    {
+//    public static int add(int x,int y)
+//    {
+//        return x + y;
+//    }
+//    public static int sub(int x,int y)
+//    {
+//        return x - y;
+//    }
+//    // what is going on??????
+//    public static void multiply(int x,int y)
+//    {
+//        int count = y;
+//        do {
+//            count--;
+//            multiply(x+x,y);
+//
+////            if (count == 0) {
+////                System.out.println(x);
+////
+////            }
+//        }while(count >= y);
+//        System.out.println(count);
+//
+//    }
+//    public static double divide(double x,double y)
+//    {
+//        return x / y;
+//    }
+//    public static boolean modulo(double x,double m)
+//    {
+//        return x % m == 0;
+//    }
 
-    }
-    public static double divide(double x,double y)
+//    public static void getInteger()
+//    {
+//        Scanner scanner = new Scanner(System.in);
+////
+//        System.out.print("Enter number from 1 - 10: ");
+//        int userInput = scanner.nextInt(); // input as int
+//        if(userInput >= 1 & userInput <= 10)
+//        {
+//            System.out.println("You entered: " + userInput + " your not that dumb thank you");
+//        }
+//        else{
+//            System.out.println("cant you read?");
+//            getInteger();
+//        }
+//    }
+    public static void calcFactorial()
     {
-        return x / y;
-    }
-    public static boolean modulo(double x,double m)
-    {
-        return x % m == 0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number from 1 - 10: ");
+        int userInput = scanner.nextInt(); // input as int
+        int i,fact=1; // what is this declaration????????/
+        for(i=1;i<=userInput;i++){
+            fact=fact*i;
+        }
+        System.out.println(userInput+"! = "+fact);
     }
 
     public static void main(String[] args)
     {
-        System.out.println(add(2,3));
-        System.out.println(sub(2,3));
-        System.out.println(multiply(2,8));
-        System.out.println(divide(2,3));
-        System.out.println(modulo(48,3));
+        calcFactorial();
+//        getInteger();
+//        System.out.println(add(2,3));
+//        System.out.println(sub(2,3));
+//        System.out.println(multiply(2,8));
+//        System.out.println(divide(2,3));
+//        System.out.println(modulo(48,3));
     }
 }
