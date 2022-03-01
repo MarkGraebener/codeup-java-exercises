12426d6184d2f8975f4dad149acd96b7975a5b2d
MarkGraebener <mark.w.graebener@gmail.com>
2022-03-01 11:51:13 -0600

    arrayslist

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,14 +1,29 @@
+import java.util.ArrayList;
 import java.util.Arrays;
-//why cant it be a public class to run this?
-class ArraysExercise
-{
-    int[] numbers = {1, 2, 3, 4, 5};
-    String[] persons = new String[3];
-    persons[0] = "mike";
-    persons[1] = "matthew";
-    persons[2] = "mark";
 
-    public static void main(String[] args) {
-        System.out.println(Arrays.toString(numbers));
+public class ArraysExercises
+{
+//    static int[] numbers = {1, 2, 3, 4, 5};
+//    static String[] persons = new String[3];
+
+    public static String[] addPerson(String person)
+    {
+        String[] arr = {"Mark", "Matthew","Mark"};
+        int n = arr.length;
+
+        ArrayList<String> arrayList = new ArrayList<String>(Arrays.asList(arr));
+        arrayList.add(person);
+        arr = arrayList.toArray(arr);
+        return arr;
+    }
+    public static void main(String[] args)
+    {
+//        System.out.println(Arrays.toString(numbers));
+//        persons[0] = "Matthew";
+//        System.out.println("Arrays.toString(persons) = " + Arrays.toString(persons));
+        System.out.println("newPerson(\"Angela\") = " + Arrays.toString(addPerson("Angela")));
+//        System.out.println(Arrays.toString(arr));
+//        System.out.println(Arrays.toString(newArr));
+
     }
 }
