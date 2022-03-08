ccaf7549b1ed729e6e427e7596d882f456e93743
MarkGraebener <mark.w.graebener@gmail.com>
2022-03-07 22:50:44 -0600

    grocery app

--- src/GroceryListApp/GroceryApp.java
+++ src/GroceryListApp/GroceryApp.java
@@ -7,13 +7,16 @@ import java.util.Scanner;
 
 public class GroceryApp
 {
+    // creating global hash map . this app is for only one user to use?????????
+    // all share the hash map but i dont like this
     public static HashMap<String, Integer> myList  = new HashMap<>();
     public static Scanner resultsInput = new Scanner(System.in);
     public static String answer;
 
+    // not needed right now
     public static void addToList(User name, String item, int Quant)
     {
-        groceryList.put(item,Quant);
+        myList.put(item,Quant);
         System.out.println(name.getGroceryList());
 
     }
@@ -22,6 +25,7 @@ public class GroceryApp
        return new User(name, myList);
     }
 
+    // starts the app
     public static void  runAppStart()
     {
         System.out.println("Thank you for your purchase, who am i speaking with?");
@@ -34,12 +38,14 @@ public class GroceryApp
 //        HashMap<String, Integer> name  = new HashMap<>();
 
     }
+    //runs the conditional
     public static void appRun()
     {
-
+// only runs once even when called again bc answer2 changes to nextline() is alwas false
+        //need to put question above and ask for another var
         String answer2 = resultsInput.nextLine();
 
-        HashMap<String, String> groceryList = new HashMap<>();
+
         if(Objects.equals(answer2,"y"))
         {
             System.out.println("What would you like to add? Item and quantity: ");
@@ -51,12 +57,17 @@ public class GroceryApp
         }
 
     }
-public static void addingItems()
-{
-    String item = resultsInput.nextLine();
-    String quant = resultsInput.nextLine();
-    groceryList.put(item,quant);
-}
+    // adding a list item and quantity to the hash map
+    public static void addingItems()
+    {
+        String item = resultsInput.nextLine();
+        int quant = resultsInput.nextInt();
+        myList.put(item, quant);
+        // how can i get the user info ??????
+        System.out.println(addUser(answer).getGroceryList());
+        appRun();
+    }
+
     public static void main(String[] args)
     {
 //        HashMap<String, Integer> MarksList = new HashMap<>();
